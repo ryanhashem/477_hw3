b7fb40118039bd05deed105241dfca8013e94b65
Ryan Hashem <ryanohashem@yahoo.com>
2026-03-23 21:34:22 -0500

    html and javascript

---
+++ server.js
@@ -0,0 +1,11 @@
+function checkFields() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  if (!email.includes("@") || password.length < 8) {
+    alert("password must contain 8 or more characters, email must contain @.");
+    return;
+  }
+
+  alert("You are now logged in.");
+}
